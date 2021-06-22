4f936cd99903343e74c9f7e733e1b430f7e24e65
esouza12 <elanasouza4@gmail.com>
2021-06-22 15:44:24 -0400

    inserção da blade alternativas

---
+++ resources/views/objetivos/alternativas.blade.php
@@ -0,0 +1,16 @@
+@extends ('layouts.index')
+
+@section ('menu')
+	<ul class="navbar-nav mr-auto">
+		<li class="nav-item">
+			<a class="nav-link" href="/objetivos">Objetivos</a>
+		</li>
+	</ul>
+@stop
+
+@section ('conteudo')
+	<h3>Alternativas do Objetivo {{$id}}</h3>
+
+	Se for alterado...
+	
+@stop
