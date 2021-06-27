5b969ec8c59c44e284b4f450373a033d58e9ae61
esouza12 <elanasouza4@gmail.com>
2021-06-27 19:57:02 -0400

    tabela para o criterio de comprar uma moto

--- resources/views/objetivos/criterios.blade.php
+++ resources/views/objetivos/criterios.blade.php
@@ -10,8 +10,8 @@
 
 @section ('conteudo')
 	<h3>Critérios do Objetivo {{ $objetivo["id"] }} - {{ $objetivo["nome"] }}</h3>
-<!--
-	<table class="table">
+
+<!--	<table class="table">
 		<theader>
 			<tr>
 				<th>ID</th>
@@ -28,4 +28,38 @@
 		</tbody>
 	</table>
 -->
+
+<table class="table">
+  <thead class="thead-light">
+    <tr>
+    <th>ID</th>
+      <th scope="col">Custo</th>
+      <th scope="col">Cilindrada</th>
+      <th scope="col">Esportividade</th>
+    </tr>
+  </thead>
+  <tbody>
+     <tr> 
+      <th scope="row">Custo</th>
+      <td>1,000</td>
+      <td>1,710</td>
+      <td>1,442</td>
+    </tr>
+    <tr>
+      <th scope="row">Cilindrada</th>
+      <td>0,585</td>
+      <td>1,000</td>
+      <td>0,693</td>
+    </tr>
+    <tr>
+      <th scope="row">Esportividade</th>
+      <td>0,693</td>
+      <td>1,442</td>
+      <td>1,000</td>
+
+    </tr>
+  </tbody>
+</table>
+
+
 @stop
