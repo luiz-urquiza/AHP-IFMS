ff35eff9de88075242a902722313ccc4d8720ef3
Luiz Urquiza <luiz.urquiza@ifms.edu.br>
2021-12-06 18:08:32 -0400

    Alteração Report

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -13,12 +13,32 @@ class ReportController extends Controller {
         $j_criteria = AHPController::GetCriteriaJudmentsMatrix($id, 0);
 		$j_alternatives = AHPController::GetAlternativesJudmentsMatrix($id, 0);
 		
-		AHPController::Normalize($j_criteria);
+//		AHPController::Normalize($j_criteria);
 		AHPController::GetPriority($j_criteria);
 		AHPController::CheckConsistency($j_criteria);
         
         echo "<hr><b>Matrix of Criteria Judments:</b><br>";
-        print_r($j_criteria);
+        $query = Judments::
+        join('node', function ($join) {
+        $join->on('judments.id_node1', '=', 'node.id')
+        ->orOn('judments.id_node2', '=', 'node.id');
+        })
+        ->where('judments.id_node', $id)
+        ->select('node.descr')
+        ->distinct()
+        ->get();
+
+        foreach($query as $q){
+            echo "$q->descr ";
+        }
+
+        //        print_r($j_criteria);
+        foreach($j_criteria as $c){
+            foreach($c as $score){
+                printf("%.2f&nbsp;&nbsp;&nbsp;&nbsp;", $score);
+            }
+            echo "<br>";
+        }
 
         echo "<hr><b>Matrix of Alternatives Judments:</b><br>";
         print_r($j_alternatives);
