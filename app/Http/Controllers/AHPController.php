88f0b1916591cb9ce3fe2e9ee08760474b3353d9
Rafael Françozo <rvfrancozo@gmail.com>
2021-12-15 11:07:43 -0400

    teste kate

--- app/Http/Controllers/AHPController.php
+++ app/Http/Controllers/AHPController.php
@@ -1,5 +1,5 @@
 <?php
-
+//teste
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
@@ -16,11 +16,11 @@ class AHPController extends Controller {
 		for($i = 0; $i < $dim; $i++) {
 			$tmp = 0;
 			for ($j = 0; $j < $dim; $j++) {
-				$tmp = $tmp + $matrix[$j][$i];			
+				$tmp = $tmp + $matrix[$j][$i];
 			}
 			array_push($sum_cols,$tmp);
 		}
-		
+
 		$n_matrix = $matrix;
 		for($i = 0; $i < $dim; $i++) {
 			for ($j = 0; $j < $dim; $j++) {
@@ -34,7 +34,7 @@ class AHPController extends Controller {
 		$n_matrix = AHPController::normalize($julgamentos);
 		$dim = count($n_matrix);
 		$priority = array();
-		
+
 		for($i = 0; $i < $dim; $i++) {
 			$sum_line = 0;
 			for($j = 0; $j < $dim; $j++) {
@@ -50,7 +50,7 @@ class AHPController extends Controller {
 		$saaty = array(0,0,0.00001,0.5247,0.8816,1.1086,1.2479,1.3417,1.4057,1.4499,1.4854);
 		$priority = AHPController::GetPriority($julgamentos);
 		$dim = count($julgamentos);
-		
+
 		$vector = array();
 		for($i = 0; $i < $dim; $i++) {
 			$tmp = 0;
@@ -60,10 +60,10 @@ class AHPController extends Controller {
 			array_push($vector, $tmp/$priority[$i]);
 		}
 		$tmp = array_sum($vector)/count($vector);
-		
+
 		$ci = ($tmp-$dim)/($dim - 1);
 		$cr = $ci/$saaty[$dim];
-		
+
 		return $cr;
 	}
 
@@ -106,15 +106,15 @@ class AHPController extends Controller {
 		$criteria = array( array() );
 
 		//preenche a matrix de julgamentos com 1
-		for($i = 0; $i < $order; $i++) 
-			for($j = 0; $j < $order; $j++) 
+		for($i = 0; $i < $order; $i++)
+			for($j = 0; $j < $order; $j++)
 				$criteria[$i][$j] = 1;
-		
+
 		$k = 0;
 		for($i = 0; $i < $order; $i++) {
 			for($j = $i+1; $j < $order; $j++) {
 				$criteria[$i][$j] = $judments[$k]->score;
-				$criteria[$j][$i] = 1/$judments[$k]->score;				
+				$criteria[$j][$i] = 1/$judments[$k]->score;
 				$k++;
 			}
 		}
@@ -135,23 +135,23 @@ class AHPController extends Controller {
 
 		$hierarchy = array();
 
-		foreach($id_criteria as $id) 
+		foreach($id_criteria as $id)
 
 			array_push($hierarchy, AHPController::GetCriteriaJudmentsMatrix($id, 0));
-		
+
 		return($hierarchy);
 	}
 
 	public function AHP() 	{
-		
+
 		$j_criteria = AHPController::GetCriteriaJudmentsMatrix(7, 0);
 		$j_alternatives = AHPController::GetAlternativesJudmentsMatrix(7, 0);
-		
+
 		AHPController::Normalize($j_criteria);
 		AHPController::GetPriority($j_criteria);
 		AHPController::CheckConsistency($j_criteria);
 
 		print_r(AHPController::FinalPriority($j_criteria, $j_alternatives));
 		  //echo "<hr>".AHPController::CheckConsistency($j_criteria)."<hr>";
-	}	
+	}
 }
