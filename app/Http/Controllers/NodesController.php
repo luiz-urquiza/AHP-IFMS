7530764c54ba271b9e55e609f2485d89dfdf8ae2
Rafael Françozo <rvfrancozo@gmail.com>
2021-12-21 08:27:22 -0400

    Correção no UpdateScore para os critérios

--- app/Http/Controllers/NodesController.php
+++ app/Http/Controllers/NodesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Objetivo;
 use App\Models\Node;
 use App\Models\Judments;
+use App\Models\Score;
 use Illuminate\Http\Request;
 use App\Http\Controllers\AHPController;
 
@@ -232,40 +233,52 @@ class NodesController extends Controller
 
     public function UpdateScore($proxy, Request $request)
     {
+        $score = new Score;
         $data = $request->all();
         $x = array(array(array())); //id_node - id_node1 - id_node2
         $n = array();
         $up = array();
+        $scores = array();
 
         for ($i = 0; $i < $data['counter']; $i++) {
             $s = explode(";", $data['score' . $i]);
             if ($s[1] != $proxy) array_push($n, $s[1]);
             if ($s[2] != $proxy) array_push($n, $s[2]);
             array_push($up, $s[0]);
-            $x[$s[0]][$s[1]][$s[2]] = $s[3];
+            if ($s[1] < $s[2]) {
+                $id_node1 = $s[1];
+                $id_node2 = $s[2];
+            }
+            if ($s[1] > $s[2]) {
+                $id_node2 = $s[1];
+                $id_node1 = $s[2];
+            }
+            echo $s[0] . " - " . $id_node1 . " - " . $id_node2 . " = " . $s[3] . "<hr>";
+            if ($id_node1 == $proxy) {
+                $x[$s[0]][$id_node1][$id_node2] = $s[3];
+            }
+            if ($id_node2 == $proxy) {
+                $x[$s[0]][$id_node2][$id_node1] = $s[3];
+            }
+            Judments::where('id_node', $s[0])
+                ->where('id_node1', $id_node1)
+                ->where('id_node2', $id_node2)
+                ->update(['score' => $s[3]]);
         }
 
         $up = array_unique($up);
 
         for ($k = 0; $k < count($up); $k++) {
             for ($i = 0; $i < count($n); $i++) {
-                for ($j = $i; $j < count($n); $j++) {
-                    $x[$up[$k]][$n[$i]][$n[$j]] = $x[$up[$k]][$proxy][$n[$i]] * $x[$up[$k]][$proxy][$n[$j]];
-                }
-            }
-        }
-
-        array_push($n, $proxy);
-        sort($n);
-        for ($k = 0; $k < count($up); $k++) {
-            for ($i = 0; $i < count($n); $i++) {
                 for ($j = $i + 1; $j < count($n); $j++) {
-                    echo $up[$k] . " - " . $n[$i] . " - " . $n[$j] . " = " . $x[$up[$k]][$n[$i]][$n[$j]] .     "<hr color='FFFF00'>";
-                    //Atualiza o julgamento
-                    Judments::where('id_node', $up[$k])->where('id_node1', $n[$i])->where('id_node2', $n[$j])->update(['score' => ($x[$up[$k]][$n[$i]][$n[$j]])]);
+                    $z = 1 / ($x[$up[$k]][$proxy][$n[$i]] / $x[$up[$k]][$proxy][$n[$j]]);
+                    echo $up[$k]." - ".$n[$i]." - ".$n[$j]." = ".$z."<hr color='FF00FF'>";
+                    Judments::where('id_node', $up[$k])
+                        ->where('id_node1', $n[$i])
+                        ->where('id_node2', $n[$j])
+                        ->update(['score' => $z]);
                 }
             }
         }
-        return redirect('/nodes');
     }
 }
