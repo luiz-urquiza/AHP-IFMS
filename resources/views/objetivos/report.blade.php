ca324520910bc648b8ffbf8b2b58dadf90de020c
rvfrancozo <rvfrancozo@gmail.com>
2022-03-15 09:07:38 -0400

    2 gráficos

--- resources/views/objetivos/report.blade.php
+++ resources/views/objetivos/report.blade.php
@@ -84,12 +84,14 @@
 		</div>
 		</div> 
 	</div>
-
-	<div class="container">
+<hr>
+	Santos é freguês do Palmeiras
+<hr>
+<div class="container">
 	<div class="row">
 		<div class="col-md-12">
 			<div class="chart-wrapper">
-				<canvas id="myChart1"></canvas>
+				<canvas id="myChart2"></canvas>
 			</div>
 		</div>
 	</div>
@@ -97,10 +99,9 @@
 
 
 	<script>
-		const ctx = document.getElementById('myChart');
-		const myChart = new Chart(ctx, {
-			type: 'bar',
-			data: {
+
+		//Data das Alternativas
+			const data = {
 				labels: [
 					@foreach($results->getAlternatives() as $a)
 					'{{$a['descr']}}',
@@ -131,62 +132,48 @@
 					],
 					borderWidth: 1
 				}]
-			},
-			options: {
-				responsive: true,
-				maintainAspectRatio: false,
-				scales: {
-					yAxes: [{
-						ticks: {
+			};
+			
+			//Configurações Gráfico 1
+			const config = {
+				type: 'bar',
+				data,
+				options: {
+					scales: {
+						y:{
 							beginAtZero: true
 						}
-					}]
-				}
-			}
-		});
-</script>
-
-<script>
-
-	const ctx = document.getElementById('myChart1');
-	const myChart1 = new Chart(ctx, {
-		type: 'bar',
-		data: {
-			labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-			datasets: [{
-				label: '# of Votes',
-				data: [1, 19, 3, 5, 2, 3],
-				backgroundColor: [
-					'rgba(255, 99, 132, 0.2)',
-					'rgba(54, 162, 235, 0.2)',
-					'rgba(255, 206, 86, 0.2)',
-					'rgba(75, 192, 192, 0.2)',
-					'rgba(153, 102, 255, 0.2)',
-					'rgba(255, 159, 64, 0.2)'
-				],
-				borderColor: [
-					'rgba(255, 99, 132, 1)',
-					'rgba(54, 162, 235, 1)',
-					'rgba(255, 206, 86, 1)',
-					'rgba(75, 192, 192, 1)',
-					'rgba(153, 102, 255, 1)',
-					'rgba(255, 159, 64, 1)'
-				],
-				borderWidth: 1
-			}]
-		},
-		options: {
-			responsive: true,
-			maintainAspectRatio: false,
-			scales: {
-				yAxes: [{
-					ticks: {
-						beginAtZero: true
 					}
-				}]
-			}
-		}
-	});
+				}
+			};
+
+			//Configurações Gráfico 2
+			const config2 = {
+				type: 'line',
+				data,
+				options: {
+					scales: {
+						y:{
+							beginAtZero: true
+						}
+					}
+				}
+			};
+			
+			//Renderiza o Gráfico 1
+			const MyChart = new Chart( 
+			document.getElementById('myChart'),
+			config
+			);
+			
+			////Renderiza o Gráfico 1
+			const MyChart2 = new Chart( 
+			document.getElementById('myChart2'),
+			config2
+			);
+		
 </script>
+
+
 
 @stop
