1e7ea67f26a65fc6a3800676987f0f3a32d9a6dd
rvfrancozo <rvfrancozo@gmail.com>
2022-03-15 20:51:43 -0400

    gráfico duplo

--- resources/views/objetivos/report.blade.php
+++ resources/views/objetivos/report.blade.php
@@ -85,7 +85,7 @@
 		</div> 
 	</div>
 <hr>
-	Santos é freguês do Palmeiras
+O Critério mais relevante para o problema de decisão {{ $results->getObjective() }} mais importante é: 
 <hr>
 <div class="container">
 	<div class="row">
@@ -97,20 +97,23 @@
 	</div>
 </div>
 
+<hr>
+A Alternativa mais relevante para o problema de decisão {{ $results->getObjective() }} mais importante é: 
+<hr>
 
 	<script>
 
-		//Data das Alternativas
+		//Data dos Critérios
 			const data = {
 				labels: [
-					@foreach($results->getAlternatives() as $a)
+					@foreach($results->getCriteria() as $a)
 					'{{$a['descr']}}',
 					@endforeach
 					],
 				datasets: [{
 					label: '% of priority of alternatives for the objective: {{$results->getObjective()}}',
 					data: [
-						@foreach($results->getScore() as $s)
+						@foreach($results->getPriority() as $s)
 						'{{$s*100}}',
 						@endforeach
 					],
@@ -147,24 +150,53 @@
 				}
 			};
 
-			//Configurações Gráfico 2
-			const config2 = {
-				type: 'line',
-				data,
-				options: {
-					scales: {
-						y:{
-							beginAtZero: true
-						}
-					}
-				}
-			};
-			
 			//Renderiza o Gráfico 1
 			const MyChart = new Chart( 
 			document.getElementById('myChart'),
 			config
 			);
+						
+
+			//Data das Alternativas
+			const data2 = {
+				labels: [
+					@foreach($results->getAlternatives() as $a)
+					'{{$a['descr']}}',
+					@endforeach
+					],
+				datasets: [{
+					label: '% of priority of alternatives for the objective: {{$results->getObjective()}}',
+					data: [
+						@foreach($results->getScore() as $s)
+						'{{$s*100}}',
+						@endforeach
+					],
+					backgroundColor: [
+						'rgba(255, 99, 132, 0.2)',
+						'rgba(54, 162, 235, 0.2)',
+						'rgba(255, 206, 86, 0.2)',
+						'rgba(75, 192, 192, 0.2)',
+						'rgba(153, 102, 255, 0.2)',
+						'rgba(255, 159, 64, 0.2)'
+					],
+					borderColor: [
+						'rgba(255, 99, 132, 1)',
+						'rgba(54, 162, 235, 1)',
+						'rgba(255, 206, 86, 1)',
+						'rgba(75, 192, 192, 1)',
+						'rgba(153, 102, 255, 1)',
+						'rgba(255, 159, 64, 1)'
+					],
+					borderWidth: 1
+				}]
+			};
+
+			//Configurações Gráfico 2
+			const config2 = {
+				type: 'bar',
+				data: data2,
+				options: {}
+			};
 			
 			////Renderiza o Gráfico 1
 			const MyChart2 = new Chart( 
